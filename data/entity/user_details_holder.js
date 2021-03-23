72a52921334e97689865440e6b5d1c4fa6c4a16e
Robert Anizoba <robert@bleeptv.co>
2021-03-23 13:19:56 +0100

    B2MVE-940 - Hash user ids with crypto library

--- data/entity/user_details_holder.js
+++ data/entity/user_details_holder.js
@@ -1,7 +1,19 @@
+const crypto = require('crypto');
+
+/**
+ * Hold user specific data to perform music engagement network requests and data persistence operations 
+ */
 class UserDetailsHolder {
+
+    /**
+     * Initialize new instance of User details holder
+     * @param {String} access_token 
+     * @param {String} userId 
+     */
     constructor(access_token, userId){
         this.access_token = access_token;
         this.userId = userId;
+        this.hashedUserId = crypto.createHash('md5').update(userId).digest('hex');
     }
 }
 
