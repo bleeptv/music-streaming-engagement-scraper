7d7fc92593bbcaf95180493f3dc69da890ad62ed
Robert Anizoba <robert@bleeptv.co>
2021-03-26 08:02:50 +0100

    B2MVE-982 - Update the dataset persistence manager with the local file manager, update the server file with async folder generation

--- persistence/dataset_manager_instance.js
+++ persistence/dataset_manager_instance.js
@@ -3,6 +3,8 @@ const AmazonS3FileManager = require('./sdk/aws/amazon_s3_file_manager');
 const awsSdkInstance = require('./sdk/aws/aws-instance');
 const amazonS3FileManager = new AmazonS3FileManager(awsSdkInstance);
 const folderPath = "datasets";
+const LocalFilesystemManager = require('./local_file_system_manager');
+const localFilesystemManager = new LocalFilesystemManager();
 
-const dataManager = new DatasetPersistenceManager(folderPath, amazonS3FileManager);
+const dataManager = new DatasetPersistenceManager(folderPath, amazonS3FileManager, localFilesystemManager);
 module.exports = dataManager;
